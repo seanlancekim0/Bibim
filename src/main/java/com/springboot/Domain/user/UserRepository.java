6ab1a562d013c567c2fe21f945823742bf0ea92a
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2023-07-13 15:03:36 +0900

    에러 해결!

---
+++ src/main/java/com/springboot/Domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.Domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -2,6 +2,9 @@ package com.springboot.web;
 
 import com.springboot.service.Posts.PostsService;
 import com.springboot.web.dto.PostsResponseDto;
+import com.springboot.security.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,20 +12,24 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RequiredArgsConstructor
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
-
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-    // "/"로 GET 요청이 들어오면 index.mustache를 띄워줌
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        //getAttribute 메소드가 없어서 error return, 화면을 출력할 때 필요한 index를 받지 못해서 서버가
+        // 계속해서 메소드를 실행하다가 redirect가 너무 많아 오류 화면 출력
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
-    // "/posts/save"로 GET 요청이 들어오면 posts-save.mustache를 띄워줌
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
@@ -31,14 +38,9 @@ public class IndexController {
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
+        model.addAttribute("post",dto);
 
         return "posts-update";
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
-    public Long delete(@PathVariable Long id) {
-        postsService.delete(id);
-        return id;
-    }
 }
