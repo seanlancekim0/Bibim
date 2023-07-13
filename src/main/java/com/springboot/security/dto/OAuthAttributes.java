afa35085424028a4734b793d674e83a38b13046b
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2023-07-13 16:46:11 +0900

    카카오 추가

--- src/main/java/com/springboot/security/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/security/dto/OAuthAttributes.java
@@ -25,7 +25,7 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-
+        if(registrationId.equals("naver")) return ofNaver(userNameAttributeName, attributes);
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,6 +34,17 @@ public class OAuthAttributes {
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_iamge"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
